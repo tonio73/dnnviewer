#3 : fix issue with layer gradient minimax graph

dnnviewer/bridge/tensorflow_datasets.py
@@ -2,6 +2,8 @@ from ..TestData import TestData
 
 from tensorflow import keras
 
+import numpy as np
+
 
 def keras_load_test_data(dataset_name, test_data: TestData):
     """ Load dataset using Keras, return a sample of the test """
@@ -20,7 +22,7 @@ def keras_load_test_data(dataset_name, test_data: TestData):
     if dataset_name in _datasets:
         dataset = _datasets[dataset_name]
         (_, _), (x_test, y_test) = dataset[0].load_data()
-        test_data.set(x_test, y_test, dataset[1], dataset[2])
+        test_data.set(x_test, np.squeeze(y_test), dataset[1], dataset[2])
 
 
 def keras_test_data_listing():