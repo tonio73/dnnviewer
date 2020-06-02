7c24e78a0ed43c997f9cdcb79c66dfd9311c76bd
Antoine Hue <antoine.hue@gmail.com>
2020-06-02 15:25:38 +0200

    #37 nicer handling of server restart on /network-view page => error message

--- dnnviewer/main_network_view.py
+++ dnnviewer/main_network_view.py
@@ -2,9 +2,11 @@ from . import AbstractDashboard, TestData
 from .bridge import AbstractModelSequence
 from .Grapher import Grapher
 from .panes import top, center, bottom
+from .widgets import font_awesome
 
 import dash_bootstrap_components as dbc
-
+import dash_html_components as html
+import dash_core_components as dcc
 
 class MainNetworkView(AbstractDashboard):
 
@@ -26,19 +28,35 @@ class MainNetworkView(AbstractDashboard):
         self.grapher = grapher
         # Panes
         self.panes = [top.TopPane(enable_navigation), center.CenterPane(), bottom.BottomPane()]
+        self.enable_navigation = enable_navigation
 
     def layout(self, has_request):
         if has_request:
-
             # Prepare rendering of panes
             [pane.render(self.grapher) for pane in self.panes]
 
-        # Layout
-        content = dbc.Container([
-            *[pane.get_layout(self.model_sequence, self.grapher, self.test_data) for pane in self.panes]
-        ], fluid=True)
+            # Layout
+            if self.model_sequence.number_epochs == 0:
+                if self.enable_navigation:
+                    return_selection = dcc.Link(html.H3([font_awesome.icon('arrow-left'),
+                                                         html.Span('Back to model selection', style={'margin-left': '15px'})]), href='/')
+                else:
+                    return_selection = html.P()
 
-        return content
+                return dbc.Container([html.H1([font_awesome.icon('binoculars'),
+                                               html.Span('Deep Neural Network Viewer',
+                                                         style={'marginLeft': '15px'})]),
+                                      html.H2('No model loaded'),
+                                      return_selection
+                                      ])
+            else:
+                return dbc.Container([
+                    *[pane.get_layout(self.model_sequence, self.grapher, self.test_data) for pane in self.panes]
+                ], fluid=True)
+        else:
+            return dbc.Container([
+                *[pane.get_layout(self.model_sequence, self.grapher, self.test_data) for pane in self.panes]
+            ], fluid=True)
 
     def callbacks(self):
 
